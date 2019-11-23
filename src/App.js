bf7d1e7b90b95be977a74e46e2057f808c354c76
Risti.nurfatiah 49 <fatiah2509@yahoo.com>
2019-11-23 14:50:55 +0700

    add router, add about, add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import Background from './bg.png';
 import Chevron from './down-chevron.png';
-
+import Foto from "./assets/img/habib.jpg";
+import {Header, Button} from "./components";
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 class App extends Component {
   constructor(props) {
     super(props)
@@ -28,10 +35,29 @@ class App extends Component {
   render(){  
     const {matches, email} =this.state
     return(
+      <Router>
         <div style={styles.background}>
         <div style={styles.overlay}>
-           <div style={styles.container}>
-            <div style={styles.topText}>Risti</div>
+          <div style={{display:'flex', position:"absolute", width:"100%", backgroundColor:"rgba(255, 255, 255, 0.75)", justifyContent: 'center'}}>
+          <Link style={styles.Link} to="/"><section style={styles.section}>Home</section></Link>
+           <Link style={styles.Link} to="/about"><section style={styles.section}>about me</section></Link>
+          </div>
+         
+         <Switch>
+          <Route path="/about">
+            <div style={styles.container}>
+            <div style={{marginTop: 85, alignSelf:'center'}}>
+              <center>
+              <img src={Foto} alt="" style={{height: 168, width: 150}}/></center>
+          <div style={matches ? styles.h1 : styles.h2}>
+        hello my name is <b>Risti</b> as full-stack developer from <b>arkademy</b>
+          </div>
+          </div>
+          </div>
+          </Route>
+          <Route path="/">
+          <div style={styles.container}>
+            <Header title="Risti"/>
             <div style={matches ? styles.h1 : styles.h2}>
               This Most Simple & Yet <b>Powerful</b> <b>Way</b> To <b>Launch</b> Your Startup
               </div>
@@ -39,19 +65,23 @@ class App extends Component {
                <div style={styles.bgInput}>
                 <input onChange={(e) => this.onChangeText(e)} value={email} type="text" name="name" placeholder="Enter Your Email" style={matches ? styles.input : styles.input2}/>
             </div>
-            <div onClick={this.onButtonPress} style={styles.button}>
-              Submit
-            </div>
+            <a href="/" onClick={this.onButtonPress} style={{textDecoration: 'none'}}>
+            <Button value={email} title="DAFTAR"/>
+            </a>
           </div>
 
             <div style={styles.wrapperScroll}>
               <p style={styles.textScroll}>Scroll Down To explore</p>
-              <img alt='chevron' style={{alignSelf: 'center'}} src={Chevron} height="24" width="24"/>
+              <img alt='chevron' style={styles.icon} src={Chevron} height="24" width="24"/>
             </div>
-
-         </div>
+         </div>  
+           
+          </Route>
+        </Switch>
       </div>
-    </div>
+     </div>
+    </Router>
+    
     );
   }
 }
@@ -131,8 +161,20 @@ wrapperScroll:{
 },
 textScroll:{
   color: '#fff', maxWidth: 100, textAlign: 'center'
-
+}, 
+icon:{
+  alignSelf: 'center'
+},
+section:{
+  margin: 8,
+  padding: 8,
+  borderStyle: 'groove',
+  borderWidth: 1,
+  color: '#000'
+},
+Link:{
+  textDecoration: 'none'
 }
-}
+};
 
 export default App;
